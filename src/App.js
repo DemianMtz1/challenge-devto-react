30e27e9e33a68518a079990c12af5cd702f41be0
vargas86 <vargas.juandedios@icloud.com>
2021-06-14 20:51:28 -0500

    correccion estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Footer from './components/Footer/footer';
+import './App.scss';
 
 function App() {
   return (
     <div>
-      Hola
+      <Footer />
     </div>
   );
 }
