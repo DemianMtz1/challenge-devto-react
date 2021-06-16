3e46d9090f2b76b7a5d9ba3acf05c311829ca1f9
demian <ericdemian.martinez@gmail.com>
2021-06-16 15:06:35 -0500

    ADDED HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from './components/Footer/footer';
+import { Home } from './screens/Home'
 import './app.scss';
 
 function App() {
   return (
     <div>
+      <Home />
       <Footer />
     </div>
   );
